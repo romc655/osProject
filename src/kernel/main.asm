1d5c2302ee5386f42f550f2ca639e2144021d234
romc655 <romcs655@gmail.com>
2025-02-12 23:03:55 +0200

    implemented FAT12 reading

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,53 +1,45 @@
-org 0x7C00
-bits 16 
+org 0x0
+bits 16
+
 
 %define ENDL 0x0D, 0x0A
 
+
 start:
-	jmp main
-
-
-puts: ;put string
-	push si
-	push ax 
-
-.loop:
-	lodsb
-	or al, al
-	jz .done
-
-	mov ah, 0x0e
-	mov bh, 0
-	int 0x10
-	
-	jmp .loop
-	
-.done:
-	pop ax 
-	pop si
-	ret
-
-main:
-
-	;setup the data segments
-	mov ax, 0
-	mov ds, ax
-	mov es, ax
-
-	;setup the stack 
-	mov ss, ax
-	mov sp, 0x7C00
-
-	;print
-	mov si, test_msg
-	call puts
-	
-	hlt
+    ; print hello world message
+    mov si, msg_hello
+    call puts
 
 .halt:
-	jmp .halt
+    cli
+    hlt
 
-test_msg: db 'Hello world!', ENDL, 0
+;
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+;
+puts:
+    ; save registers we will modify
+    push si
+    push ax
+    push bx
 
-times 510-($-$$) db 0
-dw 0AA55h
+.loop:
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
+    jz .done
+
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
+    int 0x10
+
+    jmp .loop
+
+.done:
+    pop bx
+    pop ax
+    pop si    
+    ret
+
+msg_hello: db 'Hello world from KERNEL!', ENDL, 0
